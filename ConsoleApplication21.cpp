994db477e953f6f9502ef776f81fc12756a976d5
Maxim <maxim.bakal4ul@outlook.com>
2024-10-10 18:53:47 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication21.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+using namespace std;
+
+struct Point {
+    double x;
+    double y;
+};
+
+struct Rectangle {
+    Point bottomLeft;
+    Point topRight;
+};
+
+
+bool isPointInsideRectangle(const Rectangle& rect, const Point& point) {
+    return (point.x >= rect.bottomLeft.x && point.x <= rect.topRight.x &&
+        point.y >= rect.bottomLeft.y && point.y <= rect.topRight.y);
+}
+
+int main() {
+
+    Rectangle rect;
+    rect.bottomLeft = { 0.0, 0.0 };
+    rect.topRight = { 5.0, 5.0 };
+
+
+    Point p;
+    cout << "Введите координаты точки (x, y): ";
+    cin >> p.x >> p.y;
+
+
+    if (isPointInsideRectangle(rect, p)) {
+        cout << "Точка находится внутри прямоугольника." << "\n";
+    }
+    else {
+        cout << "Точка находится вне прямоугольника." << "\n";
+    }
+
+    return 0;
+}
